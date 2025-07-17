47d99a64c8a430e5c5d159c4f5aef4fdb0addbba
avrahman <avarahman.wp@gmail.com>
2025-07-17 15:22:30 +0200

    Integrate Custom Fileds in 'includes/section-singletutlayt.php' and 'single-tutlayt.php'

--- includes/section-singletutlayt.php
+++ includes/section-singletutlayt.php
@@ -1,136 +1,142 @@
-    
-        <div class="col-8 bg-success bg-opacity-10 p-3">
-
-
-            <h3>Here is the section.single.php</h3>
-
-
-            <?php
-            if (have_posts()):
-                while (have_posts()):
-                    the_post();
-            ?>
-
-
-            <h1> <?php the_title(); ?> </h1>
+<h3>Here is the section.single.php</h3>
 
 
 
-            <section>
+<div class="col-8 bg-success bg-opacity-10 p-3">
+
+
+    <?php
+    if (have_posts()):
+        while (have_posts()):
+            the_post();
+    ?>
+
+
+            <article class="col-12 col-md-6 col-lg-12  d-flex align-items-center justify-content-center">
                 <?php
-                echo get_the_date('d/m/Y h:i:s');
-                ?>
-            </section>
-
-
-            <article class="card-body d-flex align-item-center justify-content-center row">
-                <section class="col-12 col-md-6 col-lg-6  d-flex align-items-center justify-content-center">
-                <?php
-                    if (has_post_thumbnail()):
+                if (has_post_thumbnail()):
                 ?>
                     <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="img-fluid m-1 img-thumbnail" />
                 <?php
-                    else:
-                    // echo ("No images for thos post !");
-                    endif;
+                else:
+                // echo ("No images for thos post !");
+                endif;
                 ?>
-                </section>
+            </article>
+
+
+            <section>
+                <h1 class="d-flex justify-content-center"> <?php the_title(); ?> </h1>
+
+                <?php
+                echo get_the_date('d/m/Y h:i:s');
+                ?>
+
+                <?php
+                $fname = get_the_author_meta('first_name');
+                $lname = get_the_author_meta('last_name');
+                echo '<p>Inna-t-id : ' . $fname . ' ' . $lname . '</p>';
+                //the_author();
+                ?>
+            </section>
+
+            <!--
+            <article class="card-body d-flex align-item-center justify-content-center col"> -->
+            <article class="col-12 col-md-6 col-lg-12 d-flex align-items-center justify-content-center">
 
                 <section class="col-12 col-md-6 col-lg-6">
-                <?php 
+                    <?php
                     the_content();
-                ?>
+                    ?>
+                </section>
 
-
-                <ul>
-                <?php
-                    if(get_post_meta($post->ID, 'Locuteurs', true)): ?>
-                            <li>Locuteurs : <?php echo get_post_meta($post->ID, 'Locuteurs', true);?> millions </li>
-                <?php
-                    else:
-                        echo("<p>Pas de locuteurs.</p>");
-                endif; ?>
-                <?php
-                    if(get_post_meta($post->ID, 'Locuteurs', true)): ?>
-                            <li>Existe depuis : <?php echo get_post_meta($post->ID, 'Locuteurs', true);?> siècles </li>
-                <?php
-                    else:
-                        echo("<p>Pas de locuteurs.</p>");
-                endif; ?>
-                </ul>
-    
-                <?php
-                    $fname = get_the_author_meta('first_name');
-                    $lname = get_the_author_meta('last_name');
-                    echo '<p>Inna-t-id : ' . $fname . ' ' . $lname . '</p>';
-                    //the_author();
-                ?>
-
-                <?php
-                    echo get_the_date('d/m/Y h:i:s');
-                ?>
+                <section>
+                    <ul>
+                        <?php
+                        if (get_post_meta($post->ID, 'Locuteurs', true)): ?>
+                            <li>Locuteurs : <?php echo get_post_meta($post->ID, 'Locuteurs', true); ?> millions </li>
+                        <?php
+                        else:
+                            echo ("<p>Pas de locuteurs.</p>");
+                        endif; ?>
+                        <?php
+                        if (get_post_meta($post->ID, 'Age', true)): ?>
+                            <li>Existe depuis : <?php echo get_post_meta($post->ID, 'Age', true); ?> siècles </li>
+                        <?php
+                        else:
+                            echo ("<p>Ur ittwassen ara.</p>");
+                        endif; ?>
+                        <?php
+                        if (get_post_meta($post->ID, 'Reference', true)): ?>
+                            <li>Amusnaw : <?php echo get_post_meta($post->ID, 'Reference', true); ?> gar-asen </li>
+                        <?php
+                        else:
+                            echo ("<p>Ulac wid nessen.</p>");
+                        endif; ?>
+                    </ul>
                 </section>
             </article>
 
 
             <article>
-                <?php
+                <section>
+                    <?php
                     $tags = get_the_tags();
                     if ($tags):
                         foreach ($tags as $tag):
-                ?>
-                        <a href="<?php echo get_tag_link($tag->term_id); ?>" class="badge badge-success">
-                <?php 
-                        echo $tag->name;
-                ?>
-                        </a>
+                    ?>
 
+                            <a href="<?php echo get_tag_link($tag->term_id); ?>" class="badge badge-success">
+                                <?php
+                                echo $tag->name;
+                                ?>
+                            </a>
 
-                <?php
+                    <?php
                         endforeach;
                     else:
                         echo ('Ulac tag di tqertilt.');
                     endif;
                     ?>
-            </article>
-
-
-            <article>
-                <?php
+                </section>
+                <section>
+                    <?php
                     $categories = get_the_category();
                     if ($categories):
                         foreach ($categories as $cat):
-                ?>
-                        <a href="<?php echo get_category_link($cat->term_id); ?>" class="bg-success badge badge-success p-2">
-                <?php
-                        echo $cat->name;
-                ?>
-                        </a>
-                <?php
+                    ?>
+
+                            <a href="<?php echo get_category_link($cat->term_id); ?>" class="bg-success badge badge-success p-2">
+
+                                <?php
+                                echo $cat->name;
+                                ?>
+                            </a>
+
+                    <?php
                         endforeach;
                     else:
                         echo ('This article is affiliated to no category');
                     endif;
-                ?>
-            </article>
-
-
-                <?php
+                    ?>
+                </section>
+                <section>
+                    <?php
                     comments_template();
-                ?>
-
-
-                <?php
-                    endwhile;
-                ?>
-
-
-            <article>
-                <?php
-                else:
-                    echo ("No posts available !");
-                endif;
-                ?>
+                    ?>
+                </section>
             </article>
 
-        </div>
+
+        <?php
+        endwhile;
+        ?>
+
+
+    <?php
+    else:
+        echo ("No posts available !");
+    endif;
+    ?>
+
+</div>
--- single-tutlayt.php
+++ single-tutlayt.php
@@ -2,36 +2,36 @@
 get_header();
 ?>
 
-    <main class="container">
-        <div class="card-body row d-flex justify-content-evenly flex-wrap mb-3 mt-2">
+<main class="container">
+    <div class="card-body row d-flex justify-content-evenly flex-wrap mb-3 mt-2">
 
-            <p> This is the 'single-tutlayt.php' template calling the "single-content.php" section </p>
+        <p> This is the 'single-tutlayt.php' template calling the "single-content.php" section </p>
 
-            <?php
-                get_template_part("includes/section", "singletutlayt");
+        <?php
+        get_template_part("includes/section", "singletutlayt");
+        ?>
+
+        <?php wp_link_pages(); ?>
+
+        <div class="d-flex align-items-center bg-warning bg-opacity-25 col-3">
+            <?php           // Sidebar
+            if (is_active_sidebar('post_sidebar')):
+                dynamic_sidebar('post_sidebar');
+            endif;
             ?>
 
-            <?php wp_link_pages(); ?>
-
-            <div class="col-4 bg-warning bg-opacity-25 col-3">
-                <?php           // Sidebar
-                if (is_active_sidebar('post_sidebar')):
-                    dynamic_sidebar('post_sidebar');
-                endif;
-                ?>
-
-                <?php
-                if (has_post_thumbnail()):
-                ?>
-                    <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="img-fluid m-1 img-thumbnail" />
-                <?php
-                else:
-                // echo ("No images for thos post !");
-                endif;
-                ?>
-            </div>
+            <?php
+            if (has_post_thumbnail()):
+            ?>
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="img-fluid m-1 img-thumbnail" />
+            <?php
+            else:
+            // echo ("No images for thos post !");
+            endif;
+            ?>
         </div>
-    </main>
+    </div>
+</main>
 
 <?php
 get_footer();
